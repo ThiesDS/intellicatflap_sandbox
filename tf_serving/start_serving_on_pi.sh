4a0f97e0c19fddfc2c90baf25826cbb5817257c1
Sven@stagingpi <sven_thies@web.de>
2021-03-06 17:16:02 +0000

    pi adjustments

--- tf_serving/start_serving_on_pi.sh
+++ tf_serving/start_serving_on_pi.sh
@@ -6,8 +6,9 @@ docker run -p 8501:8501 -p 8500:8500 \
     --name tfserving_mobilenet \
     --mount type=bind,source=$(pwd)/models/ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8/saved_model,target=/models/resnet/1/ \
     --mount type=bind,source=$(pwd)/tf_serving/config,target=/config/ \
+    -d \
     -e MODEL_NAME=resnet \
-    -e TF_CPP_MIN_VLOG_LEVEL=2 \
-    -t tensorflow/serving \
+    -e TF_CPP_MIN_VLOG_LEVEL=4 \
+    -t emacski/tensorflow-serving:2.4.1-linux_arm_armv7-a_neon_vfpv4 \
     --enable_batching \
     --batching_parameters_file=/config/batching_config.txt
