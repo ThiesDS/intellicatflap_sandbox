1390288f1e5b96deaa91b4fb00e5a218e62d55ff
Sven Thies <sven.thies@infomotion.de>
2021-03-07 09:17:53 +0100

    Add relative path to start_serving docker run command.

--- tf_serving/start_serving.sh
+++ tf_serving/start_serving.sh
@@ -4,10 +4,10 @@ docker kill tfserving_mobilenet
 docker rm tfserving_mobilenet
 docker run -p 8501:8501 -p 8500:8500 \
     --name tfserving_mobilenet \
-    --mount type=bind,source=/Users/administrator/private/intellicatflap_analytics/analysis/models/ssd_mobilenet_v2_fpnlite_640x640_coco17_tpu-8/saved_model,target=/models/resnet/1/ \
-    --mount type=bind,source=/Users/administrator/private/intellicatflap_analytics/analysis/tf_serving/config,target=/config/ \
+    --mount type=bind,source=$(pwd)/models/ssd_mobilenet_v2_fpnlite_320x320_coco17_tpu-8/saved_model,target=/models/resnet/1/ \
+    --mount type=bind,source=$(pwd)/tf_serving/config,target=/config/ \
     -e MODEL_NAME=resnet \
     -e TF_CPP_MIN_VLOG_LEVEL=2 \
     -t tensorflow/serving \
     --enable_batching \
-    --batching_parameters_file=/config/batching_config.txt >> docker.log
+    --batching_parameters_file=/config/batching_config.txt
