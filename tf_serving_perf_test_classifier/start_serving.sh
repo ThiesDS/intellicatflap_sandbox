e4e7f9bdceacbba1778d457a181cbf47dafb422f
Sven Thies <sven.thies@infomotion.de>
2021-04-18 12:38:12 +0200

    Modify start_serving* after restructure of repo.

--- tf_serving_perf_test_classifier/start_serving.sh
+++ tf_serving_perf_test_classifier/start_serving.sh
@@ -7,6 +7,6 @@ docker run -p 8501:8501 \
     --mount type=bind,source=$(pwd)/models/cat_classifier_v0_tobivscatflap/saved_model,target=/models/cat_classifier/1/ \
     --mount type=bind,source=$(pwd)/tf_serving_perf_test_classifier/config,target=/config/ \
     -e MODEL_NAME=cat_classifier \
-    -t tensorflow/serving
+    -t tensorflow/serving \
     --enable_batching \
     --batching_parameters_file=/config/batching_config.txt
